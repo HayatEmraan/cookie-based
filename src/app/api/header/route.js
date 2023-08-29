c460f87692cf932fc873f150b284c9523d45e418
hayatemraan <emraanhasanhayat@gmail.com>
2023-08-29 21:37:12 +0600

    updated

--- src/app/api/header/route.js
+++ src/app/api/header/route.js
@@ -2,5 +2,11 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 export async function GET(req, res) {
   const header = headers();
-  return NextResponse.json({ header });
+  const authorization = header.get("authorization");
+  const token = authorization.replace("Bearer ", "");
+  const auth = `Bearer ${token}`;
+  return NextResponse.json(
+    { message: auth },
+    { headers: { authorization: auth } }
+  );
 }
